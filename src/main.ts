04cb36d306f9a3520c576c4c3eabd5c04b48c638
bucchiarone <bucchiarone@fbk.eu>
2024-01-04 11:19:16 +0100

    learning activity example

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,20 @@ WA.onInit().then(() => {
     })
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
+   
+    WA.room.area.onEnter('Activity2').subscribe(() => {
+       // const today = new Date();
+       // const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("Activity2Popup", "It's a test ", [{
+            label: "Do the Learning Activity",
+            className: "primary",
+            callback: () => {
+                openModal();
+            }
+        }]);
+    })
+
+    WA.room.area.onLeave('Activity2').subscribe(closePopup)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -25,6 +39,16 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
+
+function openModal (){
+    WA.ui.modal.openModal({
+        title: "WorkAdventure website",
+        src: 'https://workadventu.re',
+        allow: "fullscreen",
+        allowApi: true,
+        position: "center",
+    });
+}
 
 function closePopup(){
     if (currentPopup !== undefined) {
