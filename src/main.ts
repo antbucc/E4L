b14f570b51afd3dac5339b40da85b6d2a0d7f0fe
tmaog <tommasoguidolin01@gmail.com>
2024-07-01 17:57:53 +0200

    testing for demo

--- src/main.ts
+++ src/main.ts
@@ -723,19 +723,14 @@ WA.onInit()
       // If you need to send data from the first call
       try {
         console.log('area Activity3');
-        //webSite = await WA.nav.openCoWebSite('./../images/papyrusWebp2.png', true);
-        if (actualActivity.platform != 'PapyrusWeb' && actualActivity.platform !='Collaborative') {
-          console.log('wrong spot, go to another area');
-          wrongAreaFunction('BannerA3', 'Papyrus');
-          return;
-        }
-          
-        if (actualActivity.platform == 'PapyrusWeb') webSite = await WA.nav.openCoWebSite('./images/papyrusWebp1.png', true);
+        //webSite = await WA.nav.openCoWebSite('./../images/papyrusWebp2.png', true);        
+        webSite = await WA.nav.openCoWebSite('./images/papyrusWebp1.png', true);
+        /*if (actualActivity.platform == 'PapyrusWeb') 
         else if(actualActivity.platform == 'Collaborative')
           webSite = await WA.nav.openCoWebSite(
             'https://app.eraser.io/workspace/JVoolrO5JJucnQkr1tK7?origin=share',
             true
-          );
+          );*/
       } catch (error) {
         // Handle errors if the API call fails
         console.error('Failed to get API response:', error);
